406230a8b7e7eca3ae0554c548bb54e77bf995ad
Sara Romao <sara-romao@live.com>
2023-07-16 22:16:30 -0300

    feat: inclusão de código: include

---
+++ intro/inclusao.php
@@ -0,0 +1,12 @@
+<?php
+
+//include - inserir arquivos ex: menu e footer
+
+
+include "title.php";
+
+
+
+?>
+
+<h2>Valor recebido: <?php echo $valor ?></h2>
---
+++ intro/title.php
@@ -0,0 +1,8 @@
+
+<h1>Título incluido</h1>
+
+<?php
+
+$valor =5;
+
+?>
