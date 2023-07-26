a620561d0fcbfd79c12e11983517f9ec2d5b1ba1
Sara Romao <sara-romao@live.com>
2023-07-25 22:58:41 -0300

    feat: input múltiplos valores

---
+++ php-web/form.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+  <form action="">
+    <div>
+        <input type="checkbox" name="ingredientes[]" value="Tomate">Tomate
+    </div>
+    <div>
+        <input type="checkbox" name="ingredientes[]" value="Alface">Alface
+    </div>
+    <div>
+        <input type="checkbox" name="ingredientes[]" value="Rucula">Rúcula
+    </div>
+    <div>
+        <input type="submit"  value="Enviar">
+    </div>
+
+
+  </form>
+
+
+</body>
+</html>
